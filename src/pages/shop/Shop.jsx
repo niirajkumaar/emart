e3c62e32301ac9863d5773b73c40006adbcc3f08
niirajkumaar <nirajkumar.cse2020@nsec.ac.in>
2023-06-17 15:36:02 +0530

    committed

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { PRODUCTS } from "../../Products";
+import { Product } from "./Product";
+import "./shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>NIRAJ MART</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
